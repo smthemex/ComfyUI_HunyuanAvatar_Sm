f811a12f796ff290b4ba8be3a719a3a36e8864c5
smthemex <138738845+smthemex@users.noreply.github.com>
2025-06-05 20:49:39 +0800

    Update audio_adapters.py

--- hymm_sp/modules/audio_adapters.py
+++ hymm_sp/modules/audio_adapters.py
@@ -45,6 +45,7 @@ from .parallel_states import (
     all_gather,
     all_to_all_4D,
 )
+from .config import get_config
 
 class AudioProjNet2(ModelMixin):
     """Audio Projection Model
@@ -111,7 +112,8 @@ class AudioProjNet2(ModelMixin):
         audio_embeds = audio_embeds.view(batch_size, window_size * blocks * channels)
         #print(f"audio_embeds.dtype: {audio_embeds.dtype}") #half16
         #print(f"self.proj1.weight.dtype: {self.proj1.weight.dtype}") #float32
-        audio_embeds = audio_embeds.to(dtype=self.proj1.weight.dtype) #NEED check if this is needed
+        if get_config():
+            audio_embeds = audio_embeds.to(dtype=self.proj1.weight.dtype) #NEED check if this is needed
         audio_embeds = torch.relu(self.proj1(audio_embeds))
         audio_embeds = torch.relu(self.proj2(audio_embeds))
 
