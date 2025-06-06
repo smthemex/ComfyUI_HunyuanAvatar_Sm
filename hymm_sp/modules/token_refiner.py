e7da059360bf6d00e31a3da5be52f123473bba74
smthemex <138738845+smthemex@users.noreply.github.com>
2025-06-06 16:27:49 +0800

    Update token_refiner.py

--- hymm_sp/modules/token_refiner.py
+++ hymm_sp/modules/token_refiner.py
@@ -11,7 +11,7 @@ from .embed_layers import TimestepEmbedder, TextProjection
 from .attn_layers import attention
 from .mlp_layers import MLP
 from .modulate_layers import apply_gate
-from .config import get_config
+
 
 class IndividualTokenRefinerBlock(nn.Module):
     def __init__(
@@ -194,7 +194,8 @@ class SingleTokenRefiner(nn.Module):
         t: torch.LongTensor,
         mask: Optional[torch.LongTensor] = None,
     ):
-        if get_config()[1]:
+        cpu_offload = False
+        if cpu_offload:
             timestep_aware_representations = self.t_embedder(t).to(dtype=x.dtype) #  t float32须转化fp16,c混合精度可能报错 
             #print(x.dtype) #输入的bf16 float32 和int64的三种数据类型, self.c_embedder,self.input_embedder已是fp16
         else:
@@ -206,7 +207,7 @@ class SingleTokenRefiner(nn.Module):
             context_aware_representations = (
                 (x * mask_float).sum(dim=1) / mask_float.sum(dim=1)
             )
-        if get_config()[1]:
+        if cpu_offload:
             context_aware_representations=  context_aware_representations.to(dtype=x.dtype) # fp8模式下须转化fp16,不然self.c_embedder混合精度报错 
         context_aware_representations = self.c_embedder(context_aware_representations)
         c = timestep_aware_representations + context_aware_representations
